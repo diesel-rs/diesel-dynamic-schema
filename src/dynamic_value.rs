79d3a122a4bcb8868687337dec6e181944d11bd7
razican <iban.eguia@cern.ch>
2020-02-05 15:28:57 +0100

    Implemented clone with a derive, also for `DynamicRow`

--- src/dynamic_value.rs
+++ src/dynamic_value.rs
@@ -15,27 +15,15 @@ impl diesel::sql_types::HasSqlType<Any> for diesel::mysql::Mysql {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct DynamicRow<I> {
     values: Vec<I>,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct NamedField<I> {
     pub name: String,
     pub value: I,
-}
-
-impl<I> Clone for NamedField<I>
-where
-    I: Clone,
-{
-    fn clone(&self) -> Self {
-        Self {
-            name: self.name.clone(),
-            value: self.value.clone(),
-        }
-    }
 }
 
 impl<I> DynamicRow<I> {
